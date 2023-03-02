84119d6807d22c79ae424dfaa9b4f41d03a81549
Enid <nekon689@gmail.com>
2023-03-02 10:43:29 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,9 @@ const program = new Command();
 program
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>', 'output format')
+    .argument('filepath1')
+    .argument('filepath2');
+
 
 program.parse();
